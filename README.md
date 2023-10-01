963a08f5491f90dc0eadeb16f67dfd12b737b363
Mazhar Zandsalimi <m47h4r@gmail.com>
2023-10-01 08:01:58 +0330

    feat(header): add GitHub logo and description icon

--- README.md
+++ README.md
@@ -10,14 +10,15 @@ This is a web app inspired by Sarah Drasner's task prioritizing technique.
 - [x] add colors and make things pretty
 - [x] handle drag and drop tasks into their category
 - [x] deploy to GitHub pages
+- [x] add GitHub logo linked to the repo in the header
+- [-] Change description title to html5 dialog
 - [ ] make task disabled (not draggable & not addable via any method) if it appears in all categories
 - [ ] add button to remove tasks
+  - [ ] use svg icon for remove category and tasks
   - [ ] must remove all mappings w/ a certain task that is removed
   - [ ] also must remove all mappings w/ a certain category that is removed
 - [ ] add reverse drag to remove from category (in addition to the current click to remove)
 - [ ] rank tasks via their number of usages (preferably increase their color saturation)
   - [ ] this might need you to make a color class that has a method to format into hsl with individual fields too
-- [ ] add GitHub logo to the repo in the header
-- [ ] add footer with year and your name
 - [ ] add keyboard support to be able to do everything via keyboard (Duolingo-like)
 - [ ] add animations (and preferably SVG animations)
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { GithubIcon } from "../assets/github";
 import { cinzel } from "../helper/font";
+import { QuestionMarkIcon } from "../assets/question-mark";
+import { useRef } from "react";
 
 const Header = () => {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  const modalOpenHandler = () => {
+    modalRef.current?.showModal();
+  };
+
+  const modalCloseHandler = () => {
+    modalRef.current?.close();
+  };
+
   return (
     <header className={`${cinzel.className} bg-gray-700`}>
-      <div className="max-w-7xl mx-auto py-2 flex justify-between">
+      <div className="max-w-7xl mx-auto py-2 flex justify-between items-center">
         <div className="text-2xl">
           Task <span className="font-medium">Prioritizer</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="h-6 w-6">
+            <Link
+              href="https://github.com/mazharz/task-prioritizer"
+              target="_blank"
+              rel="noreferrer"
+              className="text-gray-300 hover:text-gray-400"
+            >
+              <GithubIcon />
+            </Link>
+          </div>
+          <div
+            onClick={modalOpenHandler}
+            className="h-6 w-6 text-gray-300 hover:text-gray-400 cursor-pointer"
+            title="This is a technique to prioritize your todo tasks, you create categories per your core values (where you want to grow), then you assign your tasks to those said categories, the more categories they get assigned to, the higher priority they will have. This was inspired by a post by Sarah Drasner on css tricks. Made by Maz."
+          >
+            <QuestionMarkIcon />
+          </div>
+          <dialog ref={modalRef}>
+            <p>dialog here</p>
+            <button onClick={modalCloseHandler}>close</button>
+          </dialog>
         </div>
       </div>
     </header>
